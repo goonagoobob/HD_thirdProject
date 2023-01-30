fc055f9f7b4382805bf9338b274a91075cab346a
ILWOOCHO <earthisondanger@gmail.com>
2023-01-30 14:56:20 +0900

    tmp

---
+++ src/main/java/org/goonagoobob/HdThirdProejctApplication.java
@@ -0,0 +1,13 @@
+package org.goonagoobob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdThirdProejctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdThirdProejctApplication.class, args);
+	}
+
+}
