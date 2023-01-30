3089ef21920b41ba0a0e28f0800399ccf7123ce2
seeun98 <michelle5659@naver.com>
2023-01-30 14:40:37 +0900

    tmp

---
+++ src/main/java/org/goonagoobab/HdThirdProjectApplication.java
@@ -0,0 +1,13 @@
+package org.goonagoobab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdThirdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdThirdProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/goonagoobab/HdThirdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.goonagoobab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdThirdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
