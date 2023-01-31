bcabfae5f33ef4eb44da782a05abb1f6e370e422
seeun98 <michelle5659@naver.com>
2023-01-31 11:38:43 +0900

    [세은]member/login.html 추가

---
+++ src/main/java/org/goonagoobob/controller/member/memberController.java
@@ -0,0 +1,22 @@
+package org.goonagoobob.controller.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@Log4j2
+@RequestMapping("/member/")
+public class memberController {
+	@GetMapping("/login")
+	public void memberLogin() {
+		
+	}
+	
+	@GetMapping("/join")
+	public void memberJoin() {
+		
+	}
+}
