db0392cd858b3d6305d1463ea08779ec4037e79f
seeun98 <michelle5659@naver.com>
2023-02-01 15:15:37 +0900

    [세은]myPage_비밀번호 확인, 사용자정보변경.html

---
+++ src/main/java/org/goonagoobob/controller/myPage/myPageController.java
@@ -0,0 +1,24 @@
+package org.goonagoobob.controller.myPage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@Log4j2
+@RequestMapping("/myPage/")
+public class myPageController {
+	
+	@GetMapping("/passwordCheck")
+	public void passwordCheck() {
+		
+	}
+	
+	
+	@GetMapping("/changeUserInfo")
+	public void changeUserInfo() {
+		
+	}
+}
