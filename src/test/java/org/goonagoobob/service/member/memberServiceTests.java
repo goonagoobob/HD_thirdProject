b95729d486e481c46bfade40674e7df93bcfc267
seeun98 <michelle5659@naver.com>
2023-02-13 20:52:10 +0900

    [세은]주석처리

--- src/test/java/org/goonagoobob/service/member/memberServiceTests.java
+++ src/test/java/org/goonagoobob/service/member/memberServiceTests.java
@@ -21,24 +21,27 @@ public class memberServiceTests {
 	@Autowired
 	private memberService service;
 	
+	//id로 정보 찾기
 	@Test
 	public void selectById() {
 		memberAccount mA = service.selectById("user1");
 		System.out.println(mA);
 	}
 	
+	//회원가입 테스트 
 	@Test
 	public void insertServiceTest() {
 		memberJoin mJ = new memberJoin("self3", "1111", "self3", "self3", "self3", "self3", "self2", "self2", "1998/10/19", 0);
 		System.out.println(service.joinMemberAccount(mJ));
 	}
 	
+	//이름과 생년월일로 아이디 찾기
 	@Test
 	public void findId() {
 		System.out.println(service.findId("user2", "98/10/19"));
 	}
 	
-
+	//개인정보 변경
 	@Test
 	public void updateMemberInfo() {
 		memberChangeInfo mCI = new memberChangeInfo();
@@ -50,11 +53,13 @@ public class memberServiceTests {
 		System.out.println(service.changeMemberInfo(mCI));
 	}
 	
+	//이메일 중복체크
 	@Test
 	public void emailDoubleCheck() {
 		System.out.println(service.emailDoubleCheck("user1", "user3@email"));
 	}
 	
+	//회원탈퇴
 	@Test
 	public void memberOut() {
 		System.out.println(service.memberOut("user7"));
